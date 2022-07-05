209aae01e858e454494282f2dd5bb1570deb0260
lijiapei <lijiapei@intern.douban.com>
2022-07-06 00:09:59 +0800

    完成Concurrent

--- src/tasks/Request5Concurrent.kt
+++ src/tasks/Request5Concurrent.kt
@@ -4,5 +4,18 @@ import contributors.*
 import kotlinx.coroutines.*
 
 suspend fun loadContributorsConcurrent(service: GitHubService, req: RequestData): List<User> = coroutineScope {
-    TODO()
+    val repos = service
+        .getOrgReposCallSuspend(req.org)
+        .also { logRepos(req, it) }
+        .body() ?: listOf()
+    
+    val deferreds = repos.map {repo -> 
+        async(Dispatchers.Default) {
+            service
+                .getRepoContributorsCallSuspend(req.org, repo.name)
+                .also { logUsers(repo, it) }
+                .bodyList()
+        }
+    }
+    deferreds.awaitAll().flatten().aggregate()
 }
